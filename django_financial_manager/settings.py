3850d239885366423d75059774335da98717299d
Ваше Имя <you@example.com>
2022-06-13 00:03:20 +0300

    add postgres

--- django_financial_manager/settings.py
+++ django_financial_manager/settings.py
@@ -74,10 +74,22 @@ WSGI_APPLICATION = 'django_financial_manager.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django_financial_manager',
+        'USER': 'djangouser',
+        'PASSWORD': 'qwerty12345',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
